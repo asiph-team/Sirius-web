1839a02fb6c2d2ec1d8c36fcfbf69d635ba18f13
Jorge Almonacid <jorgealmonacid24@gmail.com>
2020-08-02 10:12:16 -0400

    Added navigation config

--- src/configs/navigationConfig.js
+++ src/configs/navigationConfig.js
@@ -17,6 +17,62 @@ const navigationConfig = [
         permissions: ["superadministrador"],
         navLink: "/dashboard/enterprises"
     },
+    {
+        id: "areas",
+        title: "Areas de trabajo",
+        type: "item",
+        icon: <Icon.Box size={20} />,
+        permissions: ["jefe area"],
+        navLink: "/dashboard/areas"
+    },
+    {
+        id: "jobs",
+        title: "Puestos de trabajo",
+        type: "item",
+        icon: <Icon.Briefcase size={20} />,
+        permissions: ["administrador", "jefe area"],
+        navLink: "/dashboard/jobs"
+    },
+    {
+        id: "employees",
+        title: "Trabajadores",
+        type: "item",
+        icon: <Icon.Users size={20} />,
+        permissions: ["administrador", "jefe area"],
+        navLink: "/dashboard/employees"
+    },
+    {
+        id: "activities",
+        title: "Actividades",
+        type: "item",
+        icon: <Icon.Activity size={20} />,
+        permissions: ["administrador", "jefe area"],
+        navLink: "/dashboard/activities"
+    },
+    {
+        id: "trainings",
+        title: "Capacitaciones",
+        type: "item",
+        icon: <Icon.Clipboard size={20} />,
+        permissions: ["administrador", "jefe area"],
+        navLink: "/dashboard/trainings"
+    },
+    {
+        id: "programs",
+        title: "Programas de vigilancia",
+        type: "item",
+        icon: <Icon.Video size={20} />,
+        permissions: ["administrador", "jefe area"],
+        navLink: "/dashboard/programs"
+    },
+    {
+        id: "actions",
+        title: "Planes de acción",
+        type: "item",
+        icon: <Icon.BookOpen size={20} />,
+        permissions: ["administrador", "jefe area"],
+        navLink: "/dashboard/actions"
+    },
 ]
 
 export default navigationConfig
