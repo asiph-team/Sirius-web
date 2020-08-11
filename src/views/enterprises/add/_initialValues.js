00d546c22f6aa27e0277779d2812e956b5d2c663
Jorge Almonacid <jorgealmonacid24@gmail.com>
2020-08-11 12:32:06 -0400

    Added validation on add enterprises

--- src/views/enterprises/add/_initialValues.js
+++ src/views/enterprises/add/_initialValues.js
@@ -8,7 +8,21 @@ export const values = {
     "email": "",
     "size": "",
     "RL": "",
-    "RLC": "",
+    "CRL": "",
     "RT": "",
-    "RTC": ""
+    "CRT": ""
 }
+
+export const options = [
+    {value: '1-10', label: '1-10'},
+    {value: '11-20', label: '11-20'},
+    {value: '21-30', label: '21-30'},
+    {value: '31-40', label: '31-40'},
+    {value: '41-50', label: '41-50'},
+    {value: '51-60', label: '51-60'},
+    {value: '61-70', label: '61-70'},
+    {value: '71-80', label: '71-80'},
+    {value: '81-90', label: '81-90'},
+    {value: '91-100', label: '91-100'},
+    {value: 'Más de 100', label: 'Más de 100'}
+]
--- src/views/enterprises/add/_validation.js
+++ src/views/enterprises/add/_validation.js
@@ -1,44 +1,54 @@
 import * as Yup from 'yup'
+import rutRegex from 'rut-regex'
+
+const phoneRegExp = /^[0-9]{8}$/
 
 export const addEnterpriseSchema = Yup.object().shape({
     name: Yup.string()
-        .min(3, "El nombre de empresa debe ser mayor a 3 caractéres")
+        .min(4, "El nombre de empresa debe ser mayor a 4 caractéres")
         .max(50, "El nombre de empresa debe ser menor a 50 caractéres")
         .required("El nombre de empresa es requerido"),
     spin: Yup.string()
-        .min(5, "El giro debe ser mayor a 5 caractéres")
+        .min(4, "El giro debe ser mayor a 4 caractéres")
         .max(50, "El giro debe ser menor a 50 caractéres")
         .required("El giro es requerido"),
     heading: Yup.string()
-        .min(5, "El rubro debe ser mayor a 5 caractéres")
+        .min(4, "El rubro debe ser mayor a 4 caractéres")
         .max(50, "El rubro debe ser menor a 50 caractéres")
         .required("El rubro es requerido"),
     address: Yup.string()
         .min(10, "La dirección debe ser mayor a 10 caractéres")
         .max(50, "La dirección debe ser menor a 50 caractéres")
         .required("La dirección es requerida"),
-    phone: Yup.number()
-        .min(8, "El número debe ser mayor a 8 caractéres")
-        .max(8, "El número debe ser menor a 8 caractéres")
+    RUT: Yup.string()
+        .test('RUT-validator', 'El RUT ingresado no es válido', function(value){
+            let dot = value.indexOf('.') <= 0 ? false : true
+            let hyphen = value.indexOf('-') <= 0 ? false : true
+            if (value) return rutRegex({exact: true, dot, hyphen}).test(value)
+            return true
+        })
+        .required('El RUT es requerido'),
+    phone: Yup.string()
+        .matches(phoneRegExp, 'El teléfono ingresado no es válido')
         .required("El número es requerido"),
     email: Yup.string()
         .email("El email ingresado no es válido")
         .required("El email es requerido"),
-    size: Yup.string().required("El tamaño de la empresa es requerido"),
+    size: Yup.string()
+        .ensure()
+        .required("El tamaño de la empresa es requerido"),
     RL: Yup.string()
         .min(5, "El representante legal debe ser mayor a 5 caractéres")
         .max(50, "El representante legal debe ser menor a 50 caractéres")
         .required("El representante legal es requerido"),
-    CRL: Yup.number()
-        .min(8, "El número debe ser mayor a 8 caractéres")
-        .max(8, "El número debe ser menor a 8 caractéres")
+    CRL: Yup.string()
+        .matches(phoneRegExp, 'El teléfono ingresado no es válido')
         .required("El número es requerido"),
     RT: Yup.string()
         .min(5, "El representante legal debe ser mayor a 5 caractéres")
         .max(50, "El representante legal debe ser menor a 50 caractéres")
         .required("El representante legal es requerido"),
-    CRT: Yup.number()
-        .min(8, "El número debe ser mayor a 8 caractéres")
-        .max(8, "El número debe ser menor a 8 caractéres")
+    CRT: Yup.string()
+        .matches(phoneRegExp, 'El teléfono ingresado no es válido')
         .required("El número es requerido")
 })
