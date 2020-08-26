0c09700d87dbe399b37ec39554be5731ae2bc6cb
Jorge Almonacid <jorgealmonacid24@gmail.com>
2020-08-26 09:45:49 -0400

    Improve on list of jobs, added remove jobs

---
+++ src/views/jobs/list/_listUI.js
@@ -0,0 +1,52 @@
+import React, { useState } from 'react'
+import { Button, Card, CardBody } from 'reactstrap'
+import { Link } from 'react-router-dom'
+import { PlusCircle } from 'react-feather'
+import {
+  AlertDialog, Header, List,
+} from '../../../components/custom'
+import { headers } from './_headers'
+
+const ListUI = (props) => {
+  const { data, remove } = props
+  const [selected, setSelected] = useState({})
+  const [visibility, setVisibility] = useState({ remove: false })
+  const show = (item, type, visible = true) => {
+    setSelected(item)
+    setVisibility({ ...visibility, [type]: visible })
+  }
+  return (
+    <>
+      <Header title="Puestos de trabajo" icon="Briefcase">
+        <Link to="/dashboard/areas/add">
+          <Button color="primary">
+            <PlusCircle size={14} />
+                &nbsp;Agregar área
+          </Button>
+        </Link>
+      </Header>
+      <Card>
+        <CardBody>
+          <List
+            data={data}
+            headers={headers}
+            resource="jobs"
+            show={show}
+          />
+        </CardBody>
+      </Card>
+      {
+        visibility.remove && (
+        <AlertDialog
+          title={`¿Estás seguro de eliminar el puesto "${selected.name}"?`}
+          paragraph="Estas operación es irreversible, se eliminará toda la información respecto al puesto de trabajo."
+          callback={() => remove({ id: selected.id }, null)}
+          callbackCancel={() => setVisibility({ ...visibility, remove: false })}
+        />
+        )
+      }
+    </>
+  )
+}
+
+export default ListUI
