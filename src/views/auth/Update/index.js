d663a95cf663e892f0ae5375fab8d6dca7353426
Diego Arrieta <darrieta.lnx@gmail.com>
2020-09-07 10:26:19 -0300

    create UI

---
+++ src/views/auth/Update/index.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import { history } from '../../../history'
+import { AlertError, AlertLoading, AlertSuccess } from '../../../components/custom'
+import { usePostResources } from '../../../utility/customHooks/resources'
+import EditUI from './_editUI'
+
+const Edit = (props) => {
+  const url = 'api/v1/user/password'
+  const { data: { loading, error, items }, postData, clean } = usePostResources()
+  return (
+    <>
+      <EditUI handleSubmit={(values) => postData(values, url)} {...props} />
+      {loading && <AlertLoading message="Actualizando contraseña" />}
+      {error && <AlertError callback={() => clean()} />}
+      {items && (
+      <AlertSuccess
+        message="La contraseña ha sido actualizada."
+        callback={() => {
+          document.getElementById('form-update-password').reset()
+          clean()
+          history.goBack()
+        }}
+      />
+      )}
+    </>
+  )
+}
+
+export default Edit
