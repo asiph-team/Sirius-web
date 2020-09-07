ec7cad3b479d285fd85e09d5d40546a82918c4e7
Diego Arrieta <darrieta.lnx@gmail.com>
2020-09-07 10:23:31 -0300

    add to export updateUserPassword

--- src/components/custom/modals/index.js
+++ src/components/custom/modals/index.js
@@ -1,4 +1,5 @@
 import ContactInfoEnterprise from './_contactInfoEnterprise'
 import ContactInfoEmployee from './_contactInfoEmployee'
+import UpdateUserPassword from './_updateUserPassword'
 
-export { ContactInfoEmployee, ContactInfoEnterprise }
+export { ContactInfoEmployee, ContactInfoEnterprise, UpdateUserPassword }
