8238e2fe3a94cd0e2c0db8f3cd46b80e27a619fb
Diego Arrieta <darrieta.lnx@gmail.com>
2020-09-07 12:31:17 -0300

    fix validation text

--- src/views/auth/Update/_form/_validation.js
+++ src/views/auth/Update/_form/_validation.js
@@ -6,13 +6,13 @@ export const enterpriseSchema = Yup.object().shape({
     .max(30, 'La contraseña actual debe ser menor a 50 caractéres')
     .required('La contraseña actual es requerida'),
   new_password: Yup.string()
-    .min(6, 'La nueva ontraseña actual debe ser mayor a 6 caractéres')
+    .min(6, 'La nueva contraseña actual debe ser mayor a 6 caractéres')
     .max(30, 'La nueva contraseña actual debe ser menor a 50 caractéres')
     .required('La nueva contraseña actual es requerida'),
   confirmation: Yup.string()
-    .min(6, 'La confirmacion de contraseña actual debe ser mayor a 6 caractéres')
-    .max(30, 'La confirmacion de contraseña actual debe ser menor a 50 caractéres')
-    .oneOf([Yup.ref('new_password'), null], 'Las Contraseñas no coinciden')
-    .required('La confirmacion de contraseña actual es requerida'),
+    .min(6, 'La confirmación de contraseña actual debe ser mayor a 6 caractéres')
+    .max(30, 'La confirmación de contraseña actual debe ser menor a 50 caractéres')
+    .oneOf([Yup.ref('new_password'), null], 'Las contraseñas no coinciden')
+    .required('La confirmación de contraseña actual es requerida'),
 
 })
