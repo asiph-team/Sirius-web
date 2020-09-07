1ae0c04564abcdaa66f4183ab0d43177da875fd1
Diego Arrieta <darrieta.lnx@gmail.com>
2020-09-07 12:22:30 -0300

    #5 refactor requerida

--- src/views/auth/Update/_form/_validation.js
+++ src/views/auth/Update/_form/_validation.js
@@ -4,15 +4,15 @@ export const enterpriseSchema = Yup.object().shape({
   password: Yup.string()
     .min(6, 'La contraseña actual debe ser mayor a 6 caractéres')
     .max(30, 'La contraseña actual debe ser menor a 50 caractéres')
-    .required('La contraseña actual es requerido'),
+    .required('La contraseña actual es requerida'),
   new_password: Yup.string()
     .min(6, 'La nueva ontraseña actual debe ser mayor a 6 caractéres')
     .max(30, 'La nueva contraseña actual debe ser menor a 50 caractéres')
-    .required('La nueva contraseña actual es requerido'),
+    .required('La nueva contraseña actual es requerida'),
   confirmation: Yup.string()
     .min(6, 'La confirmacion de contraseña actual debe ser mayor a 6 caractéres')
     .max(30, 'La confirmacion de contraseña actual debe ser menor a 50 caractéres')
     .oneOf([Yup.ref('new_password'), null], 'Las Contraseñas no coinciden')
-    .required('La confirmacion de contraseña actual es requerido'),
+    .required('La confirmacion de contraseña actual es requerida'),
 
 })
