7fb0f3e89eb77755f9e19cff582c1cb30d7ea163
Diego Arrieta <darrieta.lnx@gmail.com>
2020-09-07 10:22:47 -0300

    create modal updateUserPassword

---
+++ src/components/custom/modals/_updateUserPassword.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import {
+  Container,
+  Modal,
+  ModalHeader,
+  ModalBody,
+} from 'reactstrap'
+import Edit from '../../../views/auth/Update'
+
+const ResetUserPassword = (props) => {
+  const { onClose, visibility, userData } = props
+  return (
+    <Modal
+      isOpen={visibility}
+      toggle={onClose}
+      className="modal-dialog-centered modal-md"
+    >
+      <ModalHeader toggle={onClose} className="bg-primary">
+        <small>Editar Constraseña</small>
+      </ModalHeader>
+      <ModalBody className="modal-dialog-centered">
+        <Container className="my-2">
+          <Edit userData={userData} />
+        </Container>
+      </ModalBody>
+    </Modal>
+  )
+}
+
+export default ResetUserPassword
