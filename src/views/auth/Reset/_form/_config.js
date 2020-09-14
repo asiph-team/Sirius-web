4bc4beb46b0af2ef6e26e9070bebc51ef875869f
Diego Arrieta <darrieta.lnx@gmail.com>
2020-09-14 13:15:07 -0300

    add form

---
+++ src/views/auth/Reset/_form/_config.js
@@ -0,0 +1,12 @@
+export const config = [
+  [
+    {
+      input: 'CustomInput',
+      key: 1,
+      name: 'email',
+      title: 'Correo electrónico',
+      type: 'email',
+      grid: 12,
+    },
+  ],
+]
