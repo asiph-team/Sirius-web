d71d86fe30e9321fb5cb606f3d04076ccb929a7b
Diego Arrieta <darrieta.lnx@gmail.com>
2020-09-24 15:03:10 -0300

    add api url and pagination

--- src/views/trainings/list/index.js
+++ src/views/trainings/list/index.js
@@ -3,13 +3,14 @@ import { useFetchResources } from '../../../utility/customHooks/resources'
 import { LoadingSpinner } from '../../../components/@vuexy/Spinner'
 import { Error } from '../../../components/custom'
 import ListUI from './_listUI'
+import { urlApi } from '../../../utility/helpers/consts'
 
 const TrainingsList = () => {
-  const { items: trainings, remove } = useFetchResources('/api/v1/trainings')
+  const { items: trainings, remove, pagination } = useFetchResources(`${urlApi}/api/v1/trainings`)
   const { items, loading, error } = trainings
   if (loading) return <LoadingSpinner />
   if (error) return <Error message={error} />
-  return <ListUI data={items} remove={remove} />
+  return <ListUI data={items} remove={remove} pagination={pagination} />
 }
 
 export default TrainingsList
