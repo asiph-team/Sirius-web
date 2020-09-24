aae05ba9a0420c7a14903edda6acc8d24b52fb5a
Diego Arrieta <darrieta.lnx@gmail.com>
2020-09-24 13:09:08 -0300

    add required values

--- src/views/areas/_form/index.js
+++ src/views/areas/_form/index.js
@@ -7,8 +7,10 @@ import { FormGroup, FormSubmit } from '../../../components/custom'
 import { areaSchema } from './_validation'
 import { initialValues } from './_initialValues'
 import { config } from './_config'
+import { urlApi } from '../../../utility/helpers/consts'
 
 const FormUI = (props) => {
+  const url = `${urlApi}/api/v1/areas`
   const {
     handleSubmit, placeholder, title, options,
   } = props
@@ -18,28 +20,28 @@ const FormUI = (props) => {
         <Formik
           initialValues={placeholder || initialValues}
           validationSchema={areaSchema}
-          onSubmit={(values) => handleSubmit(values)}
+          onSubmit={(values) => handleSubmit({ name: values.name, description: values.description, chief_areas_id: values.chief_areas_id }, `${urlApi}/api/v1/areas/${values.id}`)}
         >
           {() => (
             <Form id="form-areas">
               {
-              config.map((row) => (
-                <Row key={row[0].key}>
-                  {
-                    row.map((item) => {
-                      if (options && item.options) {
-                        Object.assign(item, { ...item, options })
-                      }
-                      return (
-                        <Col sm={item.grid} key={item.key}>
-                          <FormGroup {...item} />
-                        </Col>
-                      )
-                    })
-                  }
-                </Row>
-              ))
-            }
+                config.map((row) => (
+                  <Row key={row[0].key}>
+                    {
+                      row.map((item) => {
+                        if (options && item.options) {
+                          Object.assign(item, { ...item, options })
+                        }
+                        return (
+                          <Col sm={item.grid} key={item.key}>
+                            <FormGroup {...item} />
+                          </Col>
+                        )
+                      })
+                    }
+                  </Row>
+                ))
+              }
               <FormSubmit back="/dashboard/areas" title={title} />
             </Form>
           )}
