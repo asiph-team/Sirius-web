6237afd61e73d880fe0ee6bdc4c779fc5581ad9e
Diego Arrieta <darrieta.lnx@gmail.com>
2020-09-25 09:39:03 -0300

    fix url

--- src/views/areas/edit/index.js
+++ src/views/areas/edit/index.js
@@ -8,14 +8,14 @@ import { useFetchResources, usePostResources } from '../../../utility/customHook
 import EditUI from './_editUI'
 
 const Edit = (props) => {
-  const { data: { loading, error, items }, update, clean } = usePostResources()
+  const { data: { loading, error, items }, clean, update } = usePostResources()
   const { items: employees } = useFetchResources('/api/v1/employees')
   const { items: data, loading: loadingEmployees, error: errorEmployees } = employees
   if (loadingEmployees) return <LoadingSpinner />
   if (errorEmployees) return <Error message={errorEmployees} />
   return (
     <>
-      <EditUI handleSubmit={(values) => update(values, url)} {...props} employees={data} />
+      <EditUI handleSubmit={(values, url) => update(values, url)} {...props} employees={data} />
       {loading && <AlertLoading message="Actualizando área de trabajo" />}
       {error && <AlertError callback={() => clean()} />}
       {items && (
