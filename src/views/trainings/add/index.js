22d964f610a0c68bd8f44cbca777ad8f1827823a
Diego Arrieta <darrieta.lnx@gmail.com>
2020-09-26 15:56:52 -0300

    add post data

--- src/views/trainings/add/index.js
+++ src/views/trainings/add/index.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import moment from 'moment'
 import { history } from '../../../history'
 import {
   AlertError, AlertLoading, AlertSuccess, Error,
@@ -6,9 +7,10 @@ import {
 import { LoadingSpinner } from '../../../components/@vuexy/Spinner'
 import { useFetchResources, usePostResources } from '../../../utility/customHooks/resources'
 import AddUI from './_addUI'
+import { urlApi } from '../../../utility/helpers/consts'
 
 const Add = () => {
-  const url = 'api/v1/trainings'
+  const url = `${urlApi}/api/v1/trainings`
   const { data: { loading, error, items }, postData, clean } = usePostResources()
   const { items: employees } = useFetchResources('/api/v1/employees')
   const { items: data, loading: loadingEmployees, error: errorEmployees } = employees
@@ -16,18 +18,18 @@ const Add = () => {
   if (errorEmployees) return <Error message={errorEmployees} />
   return (
     <>
-      <AddUI handleSubmit={(values) => postData(values, url)} employees={data} />
+      <AddUI handleSubmit={(values) => postData({ description: values.description, end_date: moment(values.end_date).format('YYYY-MM-DD'), frequency: values.frequency, name: values.name, start_date: moment(values.start_date).format('YYYY-MM-DD') }, url)} employees={data} />
       {loading && <AlertLoading message="Almacenando capacitación" />}
       {error && <AlertError callback={() => clean()} />}
       {items && (
-      <AlertSuccess
-        message="La capacitación ha sido creada."
-        callback={() => {
-          document.getElementById('form-trainings').reset()
-          clean()
-          history.goBack()
-        }}
-      />
+        <AlertSuccess
+          message="La capacitación ha sido creada."
+          callback={() => {
+            document.getElementById('form-trainings').reset()
+            clean()
+            history.goBack()
+          }}
+        />
       )}
     </>
   )
