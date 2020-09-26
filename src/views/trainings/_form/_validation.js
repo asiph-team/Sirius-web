c1c210c7b2b3b52517c6d2c898452a758fa599d8
Diego Arrieta <darrieta.lnx@gmail.com>
2020-09-26 15:54:13 -0300

    add form field and validations

--- src/views/trainings/_form/_validation.js
+++ src/views/trainings/_form/_validation.js
@@ -8,8 +8,13 @@ export const trainingSchema = Yup.object().shape({
     .min(3, 'El nombre de la capacitación debe ser mayor a 3 caractéres')
     .max(50, 'El nombre de la capacitación debe ser mayor a 50 caractéres')
     .required('El nombre de la capacitación es requerida'),
-  date: Yup.date()
+  start_date: Yup.date()
     .min(today, 'La fecha de compromiso no debe ser menor a la fecha actual')
+    .required('La fecha de compromiso es requerida'),
+  end_date: Yup.date().default(null)
+    .when('start_date',
+      (start_date, yup) => start_date && yup.min(start_date, 'Fecha de termino no puede ser menor a la fecha de inicio'),
+    )
     .required('La fecha de compromiso es requerida'),
   frequency: Yup.string()
     .min(3, 'La frecuencia debe ser mayor a 3 caractéres')
