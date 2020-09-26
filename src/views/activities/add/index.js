ccc12c6cbc3dcce4d146afc78d558fba6880341e
Diego Arrieta <darrieta.lnx@gmail.com>
2020-09-26 12:17:22 -0300

    add url, values

--- src/views/activities/add/index.js
+++ src/views/activities/add/index.js
@@ -6,28 +6,33 @@ import {
 import { LoadingSpinner } from '../../../components/@vuexy/Spinner'
 import { useFetchResources, usePostResources } from '../../../utility/customHooks/resources'
 import AddUI from './_addUI'
+import { urlApi } from '../../../utility/helpers/consts'
 
 const Add = () => {
-  const url = 'api/v1/activities'
+  const url = `${urlApi}/api/v1/activities`
   const { data: { loading, error, items }, postData, clean } = usePostResources()
-  const { items: workstations } = useFetchResources('/api/v1/workstations')
+  const { items: workstations } = useFetchResources(`${urlApi}/api/v1/workstations`)
   const { items: data, loading: loadingWorkstations, error: errorWorkstations } = workstations
   if (loadingWorkstations) return <LoadingSpinner />
   if (errorWorkstations) return <Error message={errorWorkstations} />
   return (
     <>
-      <AddUI handleSubmit={(values) => postData(values, url)} workstations={data} />
+      {
+        data && (
+          <AddUI handleSubmit={(values) => postData({ name: values.name, description: values.description, workstations_id: [values.workstation] }, url)} workstations={data.data.data} />
+        )
+      }
       {loading && <AlertLoading message="Almacenando actividad" />}
       {error && <AlertError callback={() => clean()} />}
       {items && (
-      <AlertSuccess
-        message="La actividad ha sido creada."
-        callback={() => {
-          document.getElementById('form-activities').reset()
-          clean()
-          history.goBack()
-        }}
-      />
+        <AlertSuccess
+          message="La actividad ha sido creada."
+          callback={() => {
+            document.getElementById('form-activities').reset()
+            clean()
+            history.goBack()
+          }}
+        />
       )}
     </>
   )
