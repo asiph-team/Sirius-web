6a6b35fc45d1b06a2cc26e19e15f09299a0af8ed
Diego Arrieta <darrieta.lnx@gmail.com>
2020-09-28 14:19:46 -0300

    assign meta to name

--- src/utility/helpers/consts.js
+++ src/utility/helpers/consts.js
@@ -1,3 +1,4 @@
 export const cellRegExp = /^[0-9]{8}$/
 export const phoneRegExp = /^[0-9]{9,11}$/
 export const urlApi = 'http://104.40.54.96'
+export const profiles = { superadministrator: 'Super Administrador', administrator: 'Administrador', chief_of_area: 'Jefe de Area' }
