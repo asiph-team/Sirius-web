b05d76443a1337db72e79024b4cc58c098e5d4f8
Diego Arrieta <darrieta.lnx@gmail.com>
2020-09-28 17:39:09 -0300

    add temp fix

--- src/utility/context/Auth.js
+++ src/utility/context/Auth.js
@@ -13,7 +13,10 @@ const ContextAuth = createContext({
   handleAuthentication: () => { },
   logout: () => { },
 })
-
+const authUrl = {
+  'admin@test.com': 'admin/login',
+  'superadmin@test.comn': 'users/login'
+}
 const Auth = (props) => {
   const savedState = localStorage.getItem('user')
   const [state, dispatch] = useReducer(authReducer, savedState
@@ -28,7 +31,14 @@ const Auth = (props) => {
   const handleAuthentication = async (values) => {
     dispatch(fetchStartAuth())
     try {
-      const response = await axios.post(`${urlApi}/api/v1/users/login`, values)
+      let url = ''
+      if (values.email == 'superadmin@test.com') {
+        url = 'admin/login'
+      } else {
+        url = 'users/login'
+      }
+
+      const response = await axios.post(`${urlApi}/api/v1/${url}`, values)
       const userMeta = JSON.parse(response.data.data.user.meta)
       const user = {}
       const { access_token } = response.data.data
