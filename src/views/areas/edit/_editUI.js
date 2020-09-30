1c2797f126520251c0107decaf228061e64f5dec
Diego Arrieta <darrieta.lnx@gmail.com>
2020-09-30 18:56:06 -0300

    add form values

--- src/views/areas/edit/_editUI.js
+++ src/views/areas/edit/_editUI.js
@@ -5,10 +5,11 @@ import FormUI from '../_form'
 const EditUI = (props) => {
   const { handleSubmit, location: { state: { placeholder } }, employees } = props
   const options = employees ? employees.map((item) => ({ label: item.name, value: item.id })) : null
+  const { chief_areas_id, description, name, id } = placeholder
   return (
     <>
       <Header title="Agregar área de trabajo" icon="Box" />
-      <FormUI handleSubmit={handleSubmit} title="Editar" options={options} placeholder={placeholder} />
+      <FormUI handleSubmit={handleSubmit} title="Editar" options={options} placeholder={{ id, manager: chief_areas_id, description, name }} />
     </>
   )
 }
