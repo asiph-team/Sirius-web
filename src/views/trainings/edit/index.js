059c0c9b5eb6e33d2bfc7ec8a8993827fb4bd350
Diego Arrieta <darrieta.lnx@gmail.com>
2020-10-01 11:52:38 -0300

    add url for update

--- src/views/trainings/edit/index.js
+++ src/views/trainings/edit/index.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import moment from 'moment'
 import { history } from '../../../history'
 import {
   AlertError, AlertLoading, AlertSuccess, Error,
@@ -6,28 +7,29 @@ import {
 import { LoadingSpinner } from '../../../components/@vuexy/Spinner'
 import { useFetchResources, usePostResources } from '../../../utility/customHooks/resources'
 import EditUI from './_editUI'
+import { urlApi } from '../../../utility/helpers/consts'
 
 const Edit = (props) => {
-  const url = 'api/v1/trainings/update'
-  const { data: { loading, error, items }, postData, clean } = usePostResources()
+  const url = `${urlApi}/api/v1/trainings`
+  const { data: { loading, error, items }, update, clean } = usePostResources()
   const { items: employees } = useFetchResources('/api/v1/employees')
   const { items: data, loading: loadingEmployees, error: errorEmployees } = employees
   if (loadingEmployees) return <LoadingSpinner />
   if (errorEmployees) return <Error message={errorEmployees} />
   return (
     <>
-      <EditUI handleSubmit={(values) => postData(values, url)} {...props} employees={data} />
+      <EditUI handleSubmit={(values) => update({ description: values.description, end_data: moment(values.end_date).format('YYYY-MM-DD'), start_date: moment(values.start_date).format('YYYY-MM-DD'), frequency: values.frequency, name: values.name }, `${url}/${values.id}`)} {...props} employees={data} />
       {loading && <AlertLoading message="Actualizando capacitación" />}
       {error && <AlertError callback={() => clean()} />}
       {items && (
-      <AlertSuccess
-        message="La información de la capacitación ha sido actualizada."
-        callback={() => {
-          document.getElementById('form-trainings').reset()
-          clean()
-          history.goBack()
-        }}
-      />
+        <AlertSuccess
+          message="La información de la capacitación ha sido actualizada."
+          callback={() => {
+            document.getElementById('form-trainings').reset()
+            clean()
+            history.goBack()
+          }}
+        />
       )}
     </>
   )
