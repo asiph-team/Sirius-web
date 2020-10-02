ff94018ec9a00d07b3fe13f857c798119b307532
Diego Arrieta <darrieta.lnx@gmail.com>
2020-10-02 13:43:49 -0300

    add formatDate function

--- src/utility/helpers/functions.js
+++ src/utility/helpers/functions.js
@@ -1,3 +1,17 @@
+import moment from 'moment'
+
 export function phoneFormat(phone) {
   return phone.replace('+569 ', '').replace(/\s/g, '')
+}
+
+export const formatDate = (list, dates, format) => {
+  const newData = list.map((item) => {
+    const keys = Object.keys(item)
+    const obj = {}
+    keys.forEach((key) => {
+      dates.includes(key) ? obj[[key]] = moment(item[[key]]).format(format) : obj[key] = item[key]
+    })
+    return obj
+  })
+  return newData
 }
