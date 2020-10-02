b3b3884756980ae2c95b5d9919b02e1be5d1f59b
Diego Arrieta <darrieta.lnx@gmail.com>
2020-10-02 18:33:00 -0300

    add values for edit form

--- src/views/actions/edit/index.js
+++ src/views/actions/edit/index.js
@@ -6,28 +6,30 @@ import {
 import { LoadingSpinner } from '../../../components/@vuexy/Spinner'
 import { useFetchResources, usePostResources } from '../../../utility/customHooks/resources'
 import EditUI from './_editUI'
+import { urlApi } from '../../../utility/helpers/consts'
+import { singleDateFormatter } from '../../../utility/helpers/functions'
 
 const Edit = (props) => {
-  const url = 'api/v1/actions/update'
-  const { data: { loading, error, items }, postData, clean } = usePostResources()
-  const { items: workstations } = useFetchResources('/api/v1/employees')
+  const url = `${urlApi}/api/v1/actions`
+  const { data: { loading, error, items }, update, clean } = usePostResources()
+  const { items: workstations } = useFetchResources(`${urlApi}/api/v1/employees`)
   const { items: data, loading: loadingEmployees, error: errorEmployees } = workstations
   if (loadingEmployees) return <LoadingSpinner />
   if (errorEmployees) return <Error message={errorEmployees} />
   return (
     <>
-      <EditUI handleSubmit={(values) => postData(values, url)} {...props} employees={data} />
+      <EditUI handleSubmit={(values) => update({ name: values.name, origin: values.origin, priority: values.priority, manager_id: values.manager_id, date_initial: singleDateFormatter(values.date_initial, 'YYYY-MM-DD'), date_committed: singleDateFormatter(values.date_committed, 'YYYY-MM-DD') }, `${url}/${values.id}`)} {...props} employees={data} />
       {loading && <AlertLoading message="Almacenando plan de acción" />}
       {error && <AlertError callback={() => clean()} />}
       {items && (
-      <AlertSuccess
-        message="El plan de acción ha sido creado."
-        callback={() => {
-          document.getElementById('form-actions').reset()
-          clean()
-          history.goBack()
-        }}
-      />
+        <AlertSuccess
+          message="El plan de acción ha sido creado."
+          callback={() => {
+            document.getElementById('form-actions').reset()
+            clean()
+            history.goBack()
+          }}
+        />
       )}
     </>
   )
