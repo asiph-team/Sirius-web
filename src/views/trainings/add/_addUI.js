2a17e0ce01a364ad87cfacabca8934091af2576a
Diego Arrieta <darrieta.lnx@gmail.com>
2020-10-05 14:54:39 -0300

    add employees data from endpoint

--- src/views/trainings/add/_addUI.js
+++ src/views/trainings/add/_addUI.js
@@ -2,14 +2,15 @@ import React from 'react'
 import { Header } from '../../../components/custom'
 import FormUI from '../_form'
 import { addConfig } from '../_form/_config'
+import { trainingSchemaAdd } from '../_form/_validation'
 
 const AddUI = (props) => {
   const { handleSubmit, employees } = props
-  const options = employees ? employees.map((item) => ({ label: item.name, value: item.id })) : null
+  const options = employees ? employees.data.data.map((item) => ({ label: item.name, value: item.id })) : null
   return (
     <>
       <Header title="Agregar capacitación" icon="Clipboard" />
-      <FormUI handleSubmit={handleSubmit} title="Agregar" config={addConfig} options={options} />
+      <FormUI handleSubmit={handleSubmit} title="Agregar" validationSchema={trainingSchemaAdd} config={addConfig} options={options} />
     </>
   )
 }
