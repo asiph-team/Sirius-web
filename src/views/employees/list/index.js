f5f3a7866a021b71af7fe2933da49757d6d5cb80
Diego Arrieta <darrieta.lnx@gmail.com>
2020-10-06 10:19:39 -0300

    add endpoint

--- src/views/employees/list/index.js
+++ src/views/employees/list/index.js
@@ -3,9 +3,10 @@ import { useFetchResources } from '../../../utility/customHooks/resources'
 import { LoadingSpinner } from '../../../components/@vuexy/Spinner'
 import { Error } from '../../../components/custom'
 import ListUI from './_listUI'
+import { urlApi } from '../../../utility/helpers/consts'
 
 const EmployeesList = () => {
-  const { items: employees, remove, changeStatus } = useFetchResources('/api/v1/employees')
+  const { items: employees, remove, changeStatus } = useFetchResources(`${urlApi}/api/v1/employees?`)
   const { items, loading, error } = employees
   if (loading) return <LoadingSpinner />
   if (error) return <Error message={error} />
