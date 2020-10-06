be58819aa4ed397eafa964d6a7f24dd0160a2625
Diego Arrieta <darrieta.lnx@gmail.com>
2020-10-06 14:15:55 -0300

    add update for workstations edit closes #64

--- src/views/workstations/edit/index.js
+++ src/views/workstations/edit/index.js
@@ -3,24 +3,25 @@ import { history } from '../../../history'
 import { AlertError, AlertLoading, AlertSuccess } from '../../../components/custom'
 import { usePostResources } from '../../../utility/customHooks/resources'
 import EditUI from './_editUI'
+import { urlApi } from '../../../utility/helpers/consts'
 
 const Edit = (props) => {
-  const url = 'api/v1/workstations/update'
-  const { data: { loading, error, items }, postData, clean } = usePostResources()
+  const url = `${urlApi}/api/v1/workstations`
+  const { data: { loading, error, items }, update, clean } = usePostResources()
   return (
     <>
-      <EditUI handleSubmit={(values) => postData(values, url)} {...props} />
+      <EditUI handleSubmit={(values) => update({ name: values.name, description: values.description }, `${url}/${values.id}`)} {...props} />
       {loading && <AlertLoading message="Actualizando puesto de trabajo" />}
       {error && <AlertError callback={() => clean()} />}
       {items && (
-      <AlertSuccess
-        message="La información del puesto de trabajo ha sido actualizado."
-        callback={() => {
-          document.getElementById('form-workstations').reset()
-          clean()
-          history.goBack()
-        }}
-      />
+        <AlertSuccess
+          message="La información del puesto de trabajo ha sido actualizado."
+          callback={() => {
+            document.getElementById('form-workstations').reset()
+            clean()
+            history.goBack()
+          }}
+        />
       )}
     </>
   )
