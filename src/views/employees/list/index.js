d8dd2a28809cbb64a7b9f6c933cd50b2cf4326d7
Diego Arrieta <darrieta.lnx@gmail.com>
2020-10-07 09:25:06 -0300

    add pagination

--- src/views/employees/list/index.js
+++ src/views/employees/list/index.js
@@ -6,11 +6,11 @@ import ListUI from './_listUI'
 import { urlApi } from '../../../utility/helpers/consts'
 
 const EmployeesList = () => {
-  const { items: employees, remove, changeStatus } = useFetchResources(`${urlApi}/api/v1/employees?`)
+  const { items: employees, remove, changeStatus, pagination } = useFetchResources(`${urlApi}/api/v1/employees?`)
   const { items, loading, error } = employees
   if (loading) return <LoadingSpinner />
   if (error) return <Error message={error} />
-  return <ListUI data={items} remove={remove} changeStatus={changeStatus} />
+  return <ListUI data={items} remove={remove} changeStatus={changeStatus} pagination={pagination} />
 }
 
 export default EmployeesList
