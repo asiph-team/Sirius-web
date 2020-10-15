aa4017eaf9beaa9e7d5d8bd452bc6d87f84f07a6
Diego Arrieta <darrieta.lnx@gmail.com>
2020-10-15 20:20:01 -0300

    add date_start

--- src/views/employees/edit/_editUI.js
+++ src/views/employees/edit/_editUI.js
@@ -8,7 +8,7 @@ import FormUI from '../_form'
 const EditUI = (props) => {
   const { handleSubmit, location: { state: { placeholder } }, employees } = props
   const options = employees ? employees.data.data.map((item) => ({ label: item.name, value: item.id })) : null
-  placeholder.date_start = singleDateFormatter(placeholder.date_start, 'DD/MM/YYYY')
+  placeholder.date_start = placeholder.date_start ? singleDateFormatter(placeholder.date_start, 'DD/MM/YYYY') : ''
   placeholder.phone = phoneFormat(placeholder.phone)
   return (
     <>
