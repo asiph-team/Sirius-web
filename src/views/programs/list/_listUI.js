78722ca876aaaef28330223d2f73bb26eae560d4
Diego Arrieta <darrieta.lnx@gmail.com>
2020-10-16 20:43:53 -0300

    add list view

---
+++ src/views/programs/list/_listUI.js
@@ -0,0 +1,64 @@
+import React, { useState } from 'react'
+import { Button, Card, CardBody } from 'reactstrap'
+import { Link } from 'react-router-dom'
+import { PlusCircle } from 'react-feather'
+import {
+  AlertDialog, Can, Header, List,
+} from '../../../components/custom'
+import { headers } from './_headers'
+import PaginationSeprated from '../../../components/custom/pagination'
+
+const ListUI = (props) => {
+  const { data, remove, pagination } = props
+  const [selected, setSelected] = useState({})
+  const [visibility, setVisibility] = useState({ remove: false })
+  const show = (item, type, visible = true) => {
+    setSelected(item)
+    setVisibility({ ...visibility, [type]: visible })
+  }
+  return (
+    <>
+      <Header title="Programas de Vigilancia" icon="Video">
+        <Can rule="programs:add">
+          <Link to="/dashboard/programs/add">
+            <Button color="primary">
+              <PlusCircle size={14} />
+                &nbsp;Agregar
+            </Button>
+          </Link>
+        </Can>
+      </Header>
+      <Card>
+        <CardBody>
+          {
+            data && (
+              <List
+                data={data.data.data}
+                headers={headers}
+                resource="programs"
+                show={show}
+              />
+            )
+          }
+        </CardBody>
+      </Card>
+      {
+        visibility.remove && (
+          <AlertDialog
+            title={`¿Estás seguro de eliminar el programa de vigilancia "${selected.name}"?`}
+            paragraph="Estas operación es irreversible, se eliminará toda la información respecto al programa de vigilancia."
+            callback={() => remove({ id: selected.id }, null)}
+            callbackCancel={() => setVisibility({ ...visibility, remove: false })}
+          />
+        )
+      }
+      {
+        data && (
+          <PaginationSeprated data={data.data} pagination={pagination} />
+        )
+      }
+    </>
+  )
+}
+
+export default ListUI
