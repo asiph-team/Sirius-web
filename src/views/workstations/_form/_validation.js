cca08772e2237ee9936e34638828dc23960ef451
Diego Arrieta <darrieta.lnx@gmail.com>
2020-10-16 10:26:59 -0300

    fix workstations

--- src/views/workstations/_form/_validation.js
+++ src/views/workstations/_form/_validation.js
@@ -3,7 +3,7 @@ import * as Yup from 'yup'
 export const workstationSchema = Yup.object().shape({
   name: Yup.string()
     .min(3, 'El nombre del puesto debe ser mayor a 3 caractéres')
-    .max(50, 'El nombre del puesto debe ser menor a 50 caractéres')
+    .max(100, 'El nombre del puesto debe ser menor a 100 caractéres')
     .required('El nombre del puesto es requerido'),
   description: Yup.string()
     .min(5, 'La descripción debe ser mayor a 5 caractéres')
