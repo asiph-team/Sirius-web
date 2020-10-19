0b6f64f6151071a902ab209c1a2bf8993de1cd4d
Diego Arrieta <darrieta.lnx@gmail.com>
2020-10-19 10:36:34 -0300

    modify error component

--- src/components/custom/error/index.js
+++ src/components/custom/error/index.js
@@ -3,11 +3,16 @@ import { Alert, Row } from 'reactstrap'
 
 const Error = (props) => {
   const { message } = props
+  const values = Object.values(message.response.data.error.message)
   return (
     <Row className="m-2">
-      <Alert color="danger" className="w-100">
-        <span>{message}</span>
-      </Alert>
+      {
+        values.map((item) => (
+          <Alert color="danger" className="w-100">
+            <span>{item}</span>
+          </Alert>
+        ))
+      }
     </Row>
   )
 }
