f2d11c063bb9900da766eefdee0a60a460f2db6c
Diego Arrieta <darrieta.lnx@gmail.com>
2020-10-24 19:04:49 -0300

    add url api constants

--- src/utility/helpers/consts.js
+++ src/utility/helpers/consts.js
@@ -2,3 +2,5 @@ export const cellRegExp = /^[0-9]{8}$/
 export const phoneRegExp = /^[0-9]{9,11}$/
 export const urlApi = 'http://104.40.54.96'
 export const profiles = { superadministrator: 'Super Administrador', administrator: 'Administrador', chief_of_area: 'Jefe de Area' }
+export const updatePasswordUrl = { superadministrator: 'admin/password', administrator: 'users/password', chief_of_area: 'users/password', employees: 'users/password' }
+export const baseApiUrl = '/api/v1/'
