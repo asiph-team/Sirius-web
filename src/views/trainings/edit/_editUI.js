47db86cba3e373c57902376b51509c9f6df67e8f
Diego Arrieta <darrieta.lnx@gmail.com>
2020-10-26 00:06:41 -0300

    add default value for employees

--- src/views/trainings/edit/_editUI.js
+++ src/views/trainings/edit/_editUI.js
@@ -6,14 +6,15 @@ import { editConfig } from '../_form/_config'
 import { trainingSchemaEdit } from '../_form/_validation'
 
 const AddUI = (props) => {
-  const { handleSubmit, location: { state: { placeholder } }, employees } = props
+  const { handleSubmit, location: { state: { placeholder } }, employees, participants } = props
   placeholder.start_date = moment(placeholder.start_date, 'DD/MM/YYYY').toDate()
   placeholder.end_date = moment(placeholder.end_date, 'DD/MM/YYYY').toDate()
+  const dataParticipants = participants ? participants.data.map((item) => ({ label: item.name, value: item.id })) : null
   const options = employees ? employees.data.data.map((item) => ({ label: item.name, value: item.id })) : null
   return (
     <>
       <Header title="Actualizar capacitación" icon="Clipboard" />
-      <FormUI handleSubmit={handleSubmit} title="Editar" config={editConfig} options={options} validationSchema={trainingSchemaEdit} placeholder={placeholder} />
+      <FormUI handleSubmit={handleSubmit} title="Editar" config={editConfig} options={options} validationSchema={trainingSchemaEdit} defaultData={dataParticipants} placeholder={placeholder} />
     </>
   )
 }
