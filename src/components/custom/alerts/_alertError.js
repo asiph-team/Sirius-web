54d22521cdacb17936cb49449d8d51df69a667ba
Diego Arrieta <darrieta.lnx@gmail.com>
2020-10-27 16:06:53 -0300

    modify error alert

--- src/components/custom/alerts/_alertError.js
+++ src/components/custom/alerts/_alertError.js
@@ -2,7 +2,8 @@ import React from 'react'
 import SweetAlert from 'react-bootstrap-sweetalert'
 
 const AlertError = (props) => {
-  const { callback } = props
+  const { callback, error } = props
+  const values = Object.values(error.response.data.error.message)
   return (
     <SweetAlert
       title=""
@@ -11,6 +12,11 @@ const AlertError = (props) => {
     >
       <h1>¡Ups!</h1>
       <p>Se ha producido un error mientras se realizaba la operación.</p>
+      {
+        values && values.map((item) => (
+          <p>{item}</p>
+        ))
+      }
     </SweetAlert>
   )
 }
