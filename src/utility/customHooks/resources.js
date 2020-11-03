f6601615fe861de32760376f2353eb40699de7ae
Diego Arrieta <darrieta.lnx@gmail.com>
2020-11-03 15:37:04 -0300

    add search resource

--- src/utility/customHooks/resources.js
+++ src/utility/customHooks/resources.js
@@ -57,11 +57,19 @@ export function useFetchResources(url) {
       .catch((error) => dispatch(fetchError(error)))
   }
 
+  const search = async (param, data) => {
+    dispatch(fetchStart())
+    await axios.get(`${url}&${param}=${data}`, header)
+      .then((response) => dispatch(fetchSuccess(response.data)))
+      .catch((error) => dispatch(fetchError(error)))
+  }
+
   return {
     items,
     remove,
     changeStatus,
     pagination,
+    search,
   }
 }
 
