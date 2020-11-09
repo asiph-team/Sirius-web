27fa35ed7b8d743c2fd5b740e29082da91aea223
Diego Arrieta <darrieta.lnx@gmail.com>
2020-11-09 12:55:53 -0300

    placeholder for employees

--- src/views/employees/list/_listUI.js
+++ src/views/employees/list/_listUI.js
@@ -37,7 +37,7 @@ const ListUI = (props) => {
   return (
     <>
       <Header title="Trabajadores" icon="Users">
-        <Search placeholder="Buscar..." search={search} icon="Search" temp={temp} param="filter" />
+        <Search placeholder="Buscar nombre o rut" search={search} icon="Search" temp={temp} param="filter" />
         <Can rule="employees:add">
           <Link to="/dashboard/employees/add">
             <Button size="" color="primary">
