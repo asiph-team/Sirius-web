e9f872a49eba089b2d712516129284a790770c49
Diego Arrieta <darrieta.lnx@gmail.com>
2020-11-09 13:29:04 -0300

    modify pagination component

--- src/components/custom/pagination/index.js
+++ src/components/custom/pagination/index.js
@@ -10,7 +10,7 @@ import classnames from 'classnames'
 import { ChevronLeft, ChevronRight } from 'react-feather'
 
 const PaginationSeprated = (props) => {
-  const { data, pagination } = props
+  const { data, pagination, param, temp } = props
   const { current_page, last_page } = data
   let startPage = 0
   let endPage = 0
@@ -39,7 +39,7 @@ const PaginationSeprated = (props) => {
             {
               showArrows && (
                 <PaginationItem href="#" className="prev-item">
-                  <PaginationLink onClick={() => pagination(1, null)} href="#">
+                  <PaginationLink onClick={() => pagination(1, { temp, param })} href="#">
                     <ChevronLeft />
                     {' '}
                   </PaginationLink>
@@ -53,7 +53,7 @@ const PaginationSeprated = (props) => {
                   <PaginationItem className={classnames({
                     active: current_page === i,
                   })}>
-                    <PaginationLink onClick={() => pagination(i, null)} href="#">
+                    <PaginationLink onClick={() => pagination(i, { temp, param })} href="#">
                       {i}
                     </PaginationLink>
                   </PaginationItem>,
@@ -64,7 +64,7 @@ const PaginationSeprated = (props) => {
             {
               showArrows && (
                 <PaginationItem href="#" className="next-item">
-                  <PaginationLink onClick={() => pagination(last_page, null)} href="#" last>
+                  <PaginationLink onClick={() => pagination(last_page, { temp, param })} href="#" last>
                     <ChevronRight />
                   </PaginationLink>
                 </PaginationItem>
