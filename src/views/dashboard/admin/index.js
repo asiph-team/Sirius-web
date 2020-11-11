8be0bd50a659d16417201cb24c8f0d5ecfbaf15f
Diego Arrieta <darrieta.lnx@gmail.com>
2020-11-11 18:59:45 -0300

    data from performance

--- src/views/dashboard/admin/index.js
+++ src/views/dashboard/admin/index.js
@@ -6,12 +6,12 @@ import DashboardUI from './_dashboardUI'
 import { urlApi, baseApiUrl } from '../../../utility/helpers/consts'
 
 const Admin = () => {
-  const { items: performance, queryParams } = useFetchResources(`${urlApi + baseApiUrl}indicators`)
+  const { items: performance, indicatorsParams } = useFetchResources(`${urlApi + baseApiUrl}indicators?`)
   const { items: areas } = useFetchResources(`${urlApi + baseApiUrl}areas`)
-  const { items, loading, error } = performance
+  const { items, loading, error, temp } = performance
   if (loading || !items) return <LoadingSpinner />
   if (error) return <Error message={error} />
-  return <DashboardUI performance={items} areas={areas} queryParams={queryParams} />
+  return <DashboardUI performance={items} areas={areas} indicatorsParams={indicatorsParams} temp={temp} />
 }
 
 export default Admin
