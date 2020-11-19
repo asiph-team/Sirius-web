c099d68c1c5ea43d83f63f5626879b8d1bed7cc9
Diego Arrieta <darrieta.lnx@gmail.com>
2020-11-19 20:31:32 -0300

    controls list

---
+++ src/views/controls/list/_headers.js
@@ -0,0 +1,17 @@
+export const headers = [
+  {
+    selector: 'name',
+    name: 'Medida de control',
+    sortable: true,
+  },
+  {
+    selector: 'activity_name',
+    name: 'Actividad',
+    sortable: true,
+  },
+  {
+    selector: 'actions',
+    name: '',
+    maxWidth: '100px',
+  },
+]
---
+++ src/views/controls/list/index.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import { useFetchResources } from '../../../utility/customHooks/resources'
+import { LoadingSpinner } from '../../../components/@vuexy/Spinner'
+import { Error } from '../../../components/custom'
+import ListUI from './_listUI'
+import { urlApi, baseApiUrl } from '../../../utility/helpers/consts'
+
+const ControlsList = () => {
+  const url = `${urlApi}${baseApiUrl}control_measures`
+  const { items: controls, remove, changeStatus, pagination, search } = useFetchResources(`${urlApi}${baseApiUrl}control_measures?`)
+  const { items, loading, error, temp } = controls
+  if (loading) return <LoadingSpinner />
+  if (error) return <Error message={error} />
+  return <ListUI data={items} remove={remove} search={search} temp={temp} changeStatus={changeStatus} pagination={pagination} url={url} error={error} />
+}
+
+export default ControlsList
