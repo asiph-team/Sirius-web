c6a8a39fa8e5f8971d87bf4261d4cd98556af84e
Diego Arrieta <darrieta.lnx@gmail.com>
2020-11-19 20:29:33 -0300

    Edit

---
+++ src/views/controls/edit/_editUI.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import moment from 'moment'
+import { Header } from '../../../components/custom'
+import FormUI from '../_form'
+
+const EditUI = (props) => {
+  const { handleSubmit, location: { state: { placeholder } }, activities } = props
+  const options = activities ? activities.data.data.map((item) => ({ label: item.name, value: item.id })) : null
+  placeholder.date_start = placeholder.date_start ? moment(placeholder.date_start, 'YYYY-MM-DD').toDate() : moment(new Date(), 'YYYY-MM-DD').toDate()
+  console.log('placeholder', placeholder)
+  return (
+    <>
+      <Header title="Editar medida de control" icon="Users" />
+      <FormUI handleSubmit={handleSubmit} title="Guardar cambios" options={options} placeholder={placeholder} />
+    </>
+  )
+}
+
+export default EditUI
