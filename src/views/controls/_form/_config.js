bccc85906a9660da24c285e28f575783b380bd60
Diego Arrieta <darrieta.lnx@gmail.com>
2020-11-19 20:28:54 -0300

    add controls form

---
+++ src/views/controls/_form/_config.js
@@ -0,0 +1,31 @@
+export const config = [
+  [
+    {
+      input: 'CustomInput',
+      key: 1,
+      name: 'name',
+      title: 'Nombre medida de control*',
+      type: 'text',
+      grid: 6,
+    },
+    {
+      input: 'CustomSelect',
+      key: 7,
+      name: 'activity_id',
+      title: 'Actividad*',
+      type: 'text',
+      options: [],
+      grid: 6,
+    },
+  ],
+  [
+    {
+      input: 'CustomFileInput',
+      key: 8,
+      name: 'image',
+      title: 'Imagen*',
+      type: 'file',
+      grid: 6,
+    },
+  ],
+]
---
+++ src/views/controls/_form/_validation.js
@@ -0,0 +1,13 @@
+import * as Yup from 'yup'
+
+export const controlsSchema = Yup.object().shape({
+  name: Yup.string()
+    .min(5, 'El nombre de la medida de control debe ser mayor a 5 caractéres')
+    .max(50, 'El nombre de la medida de control debe ser menor a 50 caractéres')
+    .required('El nombre de la medida de control es requerido'),
+  activity_id: Yup.string()
+    .ensure()
+    .required('El área es requerida'),
+  image: Yup.string()
+    .required('La imagen es requerida'),
+})
