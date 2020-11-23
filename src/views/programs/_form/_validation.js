6aeec7157bfe092bdfe4d7848bdd8590cebe03cd
Diego Arrieta <darrieta.lnx@gmail.com>
2020-11-23 12:07:46 -0300

    form

--- src/views/programs/_form/_validation.js
+++ src/views/programs/_form/_validation.js
@@ -2,9 +2,9 @@ import * as Yup from 'yup'
 
 export const programsSchema = Yup.object().shape({
   name: Yup.string()
-    .min(3, 'El nombre del programa debe ser mayor a 3 caractéres')
-    .max(100, 'El nombre del programa debe ser menor a 100 caractéres')
-    .required('El nombre del programa es requerido'),
+    .min(3, 'El nombre de la vigilancia médica debe ser mayor a 3 caractéres')
+    .max(100, 'El nombre de la vigilancia médica debe ser menor a 100 caractéres')
+    .required('El nombre de la vigilancia médica es requerido'),
   description: Yup.string()
     .min(5, 'La descripción debe ser mayor a 5 caractéres')
     .max(80, 'La descripción debe ser menor a 80 caractéres')
@@ -13,9 +13,9 @@ export const programsSchema = Yup.object().shape({
 
 export const programsSchemaEdit = Yup.object().shape({
   name: Yup.string()
-    .min(3, 'El nombre del programa debe ser mayor a 3 caractéres')
-    .max(100, 'El nombre del programa debe ser menor a 100 caractéres')
-    .required('El nombre del programa es requerido'),
+    .min(3, 'El nombre de la vigilancia médica debe ser mayor a 3 caractéres')
+    .max(100, 'El nombre de la vigilancia médica debe ser menor a 100 caractéres')
+    .required('El nombre de la vigilancia médica es requerido'),
   workstations_id: Yup.string()
     .ensure()
     .required('El puesto de trabajo es requerido'),
