4dc70a3a63de7d84228e51d0b8b3b20d8ce8468b
Diego Arrieta <darrieta.lnx@gmail.com>
2020-11-23 12:07:46 -0300

    form

--- src/views/programs/_form/_config.js
+++ src/views/programs/_form/_config.js
@@ -4,7 +4,7 @@ export const config = [
       input: 'CustomInput',
       key: 1,
       name: 'name',
-      title: 'Nombre*',
+      title: 'Nombre de la vigilancia médica*',
       type: 'text',
       grid: 12,
     },
