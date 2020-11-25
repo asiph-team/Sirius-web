99227c0d93293fe74e992b4478b9c23ce03b5ecd
Diego Arrieta <darrieta.lnx@gmail.com>
2020-11-25 17:57:21 -0300

    update chief_of_area rule

--- src/configs/rules.js
+++ src/configs/rules.js
@@ -36,7 +36,6 @@ export const rules = {
     'trainings:delete',
     'programs:edit',
     'programs:delete',
-    'actions:edit',
     'actions:delete',
     'controls:edit',
     'controls:delete',
