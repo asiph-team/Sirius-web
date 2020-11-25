f5a7421ab5ec38bb6dc71849c306a314fc196fc5
Diego Arrieta <darrieta.lnx@gmail.com>
2020-11-25 17:58:45 -0300

    add new resource

--- src/views/actions/list/index.js
+++ src/views/actions/list/index.js
@@ -6,11 +6,11 @@ import ListUI from './_listUI'
 import { urlApi } from '../../../utility/helpers/consts'
 
 const ActionsList = () => {
-  const { items: actions, remove, pagination, search } = useFetchResources(`${urlApi}/api/v1/actions?`)
+  const { items: actions, remove, pagination, search, changeStatus } = useFetchResources(`${urlApi}/api/v1/actions?`)
   const { items, loading, error, temp } = actions
   if (loading) return <LoadingSpinner />
   if (error) return <Error message={error} />
-  return <ListUI data={items} remove={remove} search={search} temp={temp} pagination={pagination} />
+  return <ListUI data={items} remove={remove} search={search} temp={temp} pagination={pagination} changeStatus={changeStatus} />
 }
 
 export default ActionsList
