76279f3aad502bf7e78cc32b0feca0864708b0c0
Diego Arrieta <darrieta.lnx@gmail.com>
2020-12-01 08:36:18 -0300

    fix

--- src/components/custom/error/index.js
+++ src/components/custom/error/index.js
@@ -1,20 +1,33 @@
 import React from 'react'
 import { Alert, Row } from 'reactstrap'
+import { Link } from 'react-router-dom'
 
 const Error = (props) => {
   const { message } = props
-  const values = Object.values(message.response.data.error.message)
-  return (
-    <Row className="m-2">
-      {
-        values.map((item) => (
-          <Alert color="danger" className="w-100">
-            <span>{item}</span>
-          </Alert>
-        ))
-      }
-    </Row>
-  )
+  try {
+    const values = Object.values(message.response.data.error.message)
+    return (
+      <Row className="m-2">
+        {
+          values.map((item) => (
+            <Alert color="danger" className="w-100">
+              <span>{item}</span>
+            </Alert>
+          ))
+        }
+      </Row>
+    )
+  } catch (error) {
+    return (
+      <Row className="m-2">
+        <Alert color="danger" sm={12} md={12} lg={6}>
+          <Link to="/" style={{ textDecoration: 'none', color: '#EF476F' }}>
+            <span>Se ha producido un error</span>
+          </Link>
+        </Alert>
+      </Row>
+    )
+  }
 }
 
 export default Error
