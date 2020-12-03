0b4390e91e5f2ff0afa2630bc900a161806bfec3
Diego Arrieta <darrieta.lnx@gmail.com>
2020-12-03 10:38:43 -0300

    controls add

--- src/views/controls/add/_addUI.js
+++ src/views/controls/add/_addUI.js
@@ -3,12 +3,12 @@ import { Header } from '../../../components/custom'
 import FormUI from '../_form'
 
 const AddUI = (props) => {
-  const { handleSubmit, areas } = props
+  const { handleSubmit, areas, activity } = props
   const options = areas ? areas.data.data.map((item) => ({ label: item.name, value: item.id })) : null
   return (
     <>
       <Header title="Agregar medida de control" icon="Users" />
-      <FormUI handleSubmit={handleSubmit} title="Agregar" options={options} />
+      <FormUI handleSubmit={handleSubmit} title="Agregar" options={options} activity={activity} />
     </>
   )
 }
