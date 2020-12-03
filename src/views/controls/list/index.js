5387b6e85167445970a58f25d2638a4bfd83804c
Diego Arrieta <darrieta.lnx@gmail.com>
2020-12-03 11:31:40 -0300

    delete control

--- src/views/controls/list/index.js
+++ src/views/controls/list/index.js
@@ -8,11 +8,11 @@ import { urlApi, baseApiUrl } from '../../../utility/helpers/consts'
 const ControlsList = (props) => {
   const { location: { state } } = props
   const url = `${urlApi}${baseApiUrl}control_measures`
-  const { items: controls, remove, changeStatus, pagination, search } = useFetchResources(`${urlApi}${baseApiUrl}activities/${state.activity_id}/control_measures?`)
+  const { items: controls, removeControl, changeStatus, pagination, search } = useFetchResources(`${urlApi}${baseApiUrl}activities/${state.activity_id}/control_measures?`)
   const { items, loading, error, temp } = controls
   if (loading) return <LoadingSpinner />
   if (error) return <Error message={error} />
-  return <ListUI data={items} remove={remove} activity={state.activity_id} search={search} temp={temp} changeStatus={changeStatus} pagination={pagination} url={url} error={error} />
+  return <ListUI data={items} remove={removeControl} activity={state.activity_id} search={search} temp={temp} changeStatus={changeStatus} pagination={pagination} url={url} error={error} />
 }
 
 export default ControlsList
