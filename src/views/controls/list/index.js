033041aa19f6b239b6b6ce95ba7abeecf747c42d
Diego Arrieta <darrieta.lnx@gmail.com>
2020-12-03 10:39:42 -0300

    controls list

--- src/views/controls/list/index.js
+++ src/views/controls/list/index.js
@@ -5,13 +5,14 @@ import { Error } from '../../../components/custom'
 import ListUI from './_listUI'
 import { urlApi, baseApiUrl } from '../../../utility/helpers/consts'
 
-const ControlsList = () => {
+const ControlsList = (props) => {
+  const { location: { state } } = props
   const url = `${urlApi}${baseApiUrl}control_measures`
-  const { items: controls, remove, changeStatus, pagination, search } = useFetchResources(`${urlApi}${baseApiUrl}control_measures?`)
+  const { items: controls, remove, changeStatus, pagination, search } = useFetchResources(`${urlApi}${baseApiUrl}activities/${state.activity_id}/control_measures?`)
   const { items, loading, error, temp } = controls
   if (loading) return <LoadingSpinner />
   if (error) return <Error message={error} />
-  return <ListUI data={items} remove={remove} search={search} temp={temp} changeStatus={changeStatus} pagination={pagination} url={url} error={error} />
+  return <ListUI data={items} remove={remove} activity={state.activity_id} search={search} temp={temp} changeStatus={changeStatus} pagination={pagination} url={url} error={error} />
 }
 
 export default ControlsList
