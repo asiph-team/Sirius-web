6cb64d271842cd1e5f6483373ef4c2407d59637d
Diego Arrieta <darrieta.lnx@gmail.com>
2020-12-03 10:33:45 -0300

    activity in formsubmit

--- src/components/custom/formSubmit/index.js
+++ src/components/custom/formSubmit/index.js
@@ -5,12 +5,13 @@ import {
 import { Link } from 'react-router-dom'
 
 const FormSubmit = (props) => {
-  const { back, title, onClose } = props
+  const { back, title, onClose, activityId } = props
   return (
     <Row>
       <Col sm="12 d-flex justify-content-end">
         <div>
           {back && <Link to={back}><Button color="light">Cancelar</Button></Link>}
+          {activityId && <Link to={{ pathname: '/dashboard/controls', state: { activity_id: activityId } }}><Button color="light">Cancelar</Button></Link>}
           {onClose && <Button onClick={onClose} color="light">Cancelar</Button>}
           <Button color="primary" className="ml-1" type="submit">{title}</Button>
         </div>
