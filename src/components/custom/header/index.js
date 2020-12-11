d7f5d081551a7a0f9fec3d18d5b3c18b1a24fcd4
Diego Arrieta <darrieta.lnx@gmail.com>
2020-12-11 10:40:47 -0300

    add new header on Courses

--- src/components/custom/header/index.js
+++ src/components/custom/header/index.js
@@ -1,24 +1,7 @@
-import React from 'react'
-import { Row, Col } from 'reactstrap'
-import * as Icon from 'react-feather'
+import Header from './_header'
+import HeaderTrainingDetail from './_headerTrainingDetail'
 
-const Header = (props) => {
-  const { icon, title, children } = props
-  const IconTag = Icon[icon]
-  return (
-    <Row className="mb-2">
-      <Col sm="12" lg="12" className="d-flex align-items-center">
-        <IconTag size={20} />
-        <h3 className="font-medium-5 extension-title mb-0" data-tour="extension-title">
-          &nbsp;
-          {title}
-        </h3>
-      </Col>
-      <Col sm="12" lg="12" className="d-flex flex-wrap align-items-center justify-content-end">
-        {children}
-      </Col>
-    </Row>
-  )
+export {
+  Header,
+  HeaderTrainingDetail,
 }
-
-export default Header
