79d1e20bffce9d5fff9c1d2b2fa7c3d65eea14f2
Diego Arrieta <darrieta.lnx@gmail.com>
2020-12-11 17:17:02 -0300

    programs form

--- src/views/programs/_form/_validation.js
+++ src/views/programs/_form/_validation.js
@@ -1,6 +1,9 @@
 import * as Yup from 'yup'
 
-export const programsSchema = Yup.object().shape({
+const today = new Date()
+today.setHours(0, 0, 0, 0)
+
+export const validationSchema = Yup.object().shape({
   name: Yup.string()
     .min(3, 'El nombre de la vigilancia médica debe ser mayor a 3 caractéres')
     .max(100, 'El nombre de la vigilancia médica debe ser menor a 100 caractéres')
@@ -9,18 +12,20 @@ export const programsSchema = Yup.object().shape({
     .min(5, 'La descripción debe ser mayor a 5 caractéres')
     .max(80, 'La descripción debe ser menor a 80 caractéres')
     .required('Una descripción es requerida'),
-})
-
-export const programsSchemaEdit = Yup.object().shape({
-  name: Yup.string()
-    .min(3, 'El nombre de la vigilancia médica debe ser mayor a 3 caractéres')
-    .max(100, 'El nombre de la vigilancia médica debe ser menor a 100 caractéres')
-    .required('El nombre de la vigilancia médica es requerido'),
+  frequency: Yup.string()
+    .ensure()
+    .required('La frecuencia es requerida'),
+  start_date: Yup.date()
+    .min(today, 'La fecha de compromiso no debe ser menor a la fecha actual')
+    .required('La fecha de compromiso es requerida'),
+  end_date: Yup.date()
+    // eslint-disable-next-line camelcase
+    .when('start_date', (start_date, yup) => start_date && yup.min(start_date, 'La fecha final no puede ser antes de la inicial'))
+    .required('La fecha final es requerida'),
   workstations_id: Yup.string()
     .ensure()
     .required('El puesto de trabajo es requerido'),
-  description: Yup.string()
-    .min(5, 'La descripción debe ser mayor a 5 caractéres')
-    .max(80, 'La descripción debe ser menor a 80 caractéres')
-    .required('Una descripción es requerida'),
+  employees_id: Yup.string()
+    .ensure()
+    .required('Empleados invitados es requerido'),
 })
