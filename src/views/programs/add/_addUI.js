be28405f1ba4aa22ecbf44f0867acd18023909cb
Diego Arrieta <darrieta.lnx@gmail.com>
2020-12-11 17:17:18 -0300

    programs add

--- src/views/programs/add/_addUI.js
+++ src/views/programs/add/_addUI.js
@@ -2,15 +2,21 @@ import React from 'react'
 import { Header } from '../../../components/custom'
 import FormUI from '../_form'
 import { config } from '../_form/_config'
-import { programsSchema } from '../_form/_validation'
 
 const AddUI = (props) => {
-  const { handleSubmit, employees } = props
-  const options = employees ? employees.data.data.map((item) => ({ label: item.name, value: item.id })) : null
+  const { handleSubmit, employees, workstations } = props
+  const options = workstations ? workstations.data.data.map((item) => ({ label: item.name, value: item.id })) : null
+  const employeesList = employees ? employees.data.data.map((item) => ({ label: `${item.name} ${item.lastname}`, value: item.id })).sort((a, b) => { return a.label > b.label ? 1 : -1 }) : null
   return (
     <>
       <Header title="Agregar vigilancia médica" icon="Box" />
-      <FormUI handleSubmit={handleSubmit} title="Agregar" options={options} config={config} validationSchema={programsSchema} />
+      <FormUI
+        handleSubmit={handleSubmit}
+        title="Agregar"
+        config={config}
+        options={options}
+        employees={employeesList}
+      />
     </>
   )
 }
--- src/views/programs/add/index.js
+++ src/views/programs/add/index.js
@@ -1,32 +1,37 @@
 import React from 'react'
+import moment from 'moment'
 import { history } from '../../../history'
 import {
   AlertError, AlertLoading, AlertSuccess, Error,
 } from '../../../components/custom'
 import { LoadingSpinner } from '../../../components/@vuexy/Spinner'
-import { usePostResources } from '../../../utility/customHooks/resources'
+import { usePostResources, useFetchResources } from '../../../utility/customHooks/resources'
 import AddUI from './_addUI'
-import { urlApi } from '../../../utility/helpers/consts'
+import { urlApi, baseApiUrl } from '../../../utility/helpers/consts'
 
 const Add = () => {
   const url = `${urlApi}/api/v1/programs`
   const { data: { loading, error, items }, postData, clean } = usePostResources()
+  const { items: employees } = useFetchResources(`${urlApi}${baseApiUrl}employees?`)
+  const { items: workstations } = useFetchResources(`${urlApi}${baseApiUrl}workstations?all`)
+  const { items: data } = workstations
+  const { items: employeesData } = employees
   if (loading) return <LoadingSpinner />
   if (error) return <Error message={error} />
   return (
     <>
-      <AddUI handleSubmit={(values) => postData(values, url)} />
+      <AddUI handleSubmit={(values) => postData({ ...values, start_date: moment(values.start_date).format('YYYY-MM-DD'), end_date: moment(values.start_date).format('YYYY-MM-DD') }, url)} workstations={data} employees={employeesData} />
       {loading && <AlertLoading message="Almacenando vigilancia médica" />}
       {error && <AlertError callback={() => clean()} />}
       {items && (
-      <AlertSuccess
-        message="La vigilancia médica ha sido creada."
-        callback={() => {
-          document.getElementById('form-programs').reset()
-          clean()
-          history.goBack()
-        }}
-      />
+        <AlertSuccess
+          message="La vigilancia médica ha sido creada."
+          callback={() => {
+            document.getElementById('form-programs').reset()
+            clean()
+            history.goBack()
+          }}
+        />
       )}
     </>
   )
