b6efe36a85c803ca0aa551c1b4597be62026bc89
Diego Arrieta <darrieta.lnx@gmail.com>
2020-12-21 17:54:45 -0300

    programs

---
+++ src/views/programs/participant/_headers.js
@@ -0,0 +1,32 @@
+export const headers = [
+  {
+    selector: 'name',
+    name: 'Nombre',
+    sortable: true,
+  },
+  {
+    selector: 'rut',
+    name: 'RUT',
+    sortable: true,
+  },
+  {
+    selector: 'area',
+    name: 'Área de trabajo',
+    sortable: true,
+  },
+  {
+    selector: 'workstation',
+    name: 'Puesto de trabajo',
+    sortable: true,
+  },
+  {
+    selector: 'assistance',
+    name: 'Asistencia',
+    sortable: true,
+  },
+  {
+    selector: '_signature_',
+    name: 'Asistencia',
+    sortable: true,
+  },
+]
---
+++ src/views/programs/participant/index.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import { useFetchResources } from '../../../utility/customHooks/resources'
+import { LoadingSpinner } from '../../../components/@vuexy/Spinner'
+import { Error } from '../../../components/custom'
+import DetailUI from './_detail'
+import { urlApi, baseApiUrl } from '../../../utility/helpers/consts'
+
+const ParticipantList = (props) => {
+  const { location: { state: { item } } } = props
+  const {
+    items: trainings, remove, pagination, search, orderBy, updateSignature
+  } = useFetchResources(`${urlApi}${baseApiUrl}programs/courses/${item.id}/assistance`)
+  const {
+    items, loading, error, temp,
+  } = trainings
+  console.log('items', items)
+  if (loading) return <LoadingSpinner />
+  if (error) return <Error message={error} />
+  return (
+    <DetailUI
+      data={items}
+      training={item}
+      remove={remove}
+      ordering={orderBy}
+      search={search}
+      temp={temp}
+      pagination={pagination}
+      updateSignature={updateSignature}
+    />
+  )
+}
+
+export default ParticipantList
