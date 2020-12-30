9e7fabef7e1306e0f229aefc7decfbf5dfe42463
Diego Arrieta <darrieta.lnx@gmail.com>
2020-12-29 23:18:00 -0300

    add new header

---
+++ src/components/custom/header/_headerSign.js
@@ -0,0 +1,64 @@
+import React from 'react'
+import { Row, Col, Button, Card, CardBody } from 'reactstrap'
+import { ChevronLeft } from 'react-feather'
+import { Link } from 'react-router-dom'
+import { history } from '../../../history'
+
+const HeaderSign = (props) => {
+  const { detail, title, back } = props
+  return (
+    <Row className="mb-2">
+      <Col sm="12" lg="12" className="d-flex align-items-center justify-items-between">
+        {back ? (
+          <Link to={back}>
+            <Button color="primary">
+              <ChevronLeft size={14} />
+                &nbsp;Volver a
+                {' '}
+              {title}
+            </Button>
+          </Link>
+        ) : (
+            <Button color="primary" onClick={() => history.goBack()}>
+              <ChevronLeft size={14} />
+                &nbsp;Volver a {' '}
+              {title}
+            </Button>
+          )}
+      </Col>
+      <Col sm="12" lg="12" className="d-flex flex-wrap align-items-center mt-1">
+        <Card className="d-flex flex-wrap col-lg-12">
+          <CardBody className="col-lg-12">
+            <h1>{detail.name}</h1>
+            <h3>{detail.description}</h3>
+          </CardBody>
+          <Row className="px-2">
+            <Col>
+              <h4>
+                Fecha:
+                {' '}
+                {detail.date}
+              </h4>
+            </Col>
+            <Col>
+              <h4>
+                Invitados:
+                {' '}
+                {detail.total_invited}
+              </h4>
+            </Col>
+            <Col>
+              <h4>
+                Asistencia:
+                {' '}
+                {detail.total_assisted}
+              </h4>
+            </Col>
+          </Row>
+        </Card>
+      </Col>
+    </Row>
+  )
+}
+
+export default HeaderSign
