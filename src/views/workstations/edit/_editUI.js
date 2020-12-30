c250747c387129fb55aa1a939b4a0194969ecdc5
Diego Arrieta <darrieta.lnx@gmail.com>
2020-12-30 17:38:45 -0300

    improve on Edit workstations

--- src/views/workstations/edit/_editUI.js
+++ src/views/workstations/edit/_editUI.js
@@ -5,7 +5,7 @@ import { Editconfig } from '../_form/_config'
 
 const EditUI = (props) => {
   const { handleSubmit, location: { state: { placeholder } }, areas } = props
-  const options = areas ? areas.map((item) => ({ label: item.name, value: item.id })).sort((a, b) => { return a.label > b.label ? 1 : -1 }) : null
+  const options = areas ? areas.data.data.map((item) => ({ label: item.name, value: item.id })).sort((a, b) => { return a.label > b.label ? 1 : -1 }) : null
   return (
     <>
       <Header title="Editar puesto de trabajo" icon="Briefcase" />
