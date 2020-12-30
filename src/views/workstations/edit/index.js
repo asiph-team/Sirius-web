a39f733df3f319692d66609cdb7e519d20bddb5b
Diego Arrieta <darrieta.lnx@gmail.com>
2020-12-30 17:38:45 -0300

    improve on Edit workstations

--- src/views/workstations/edit/index.js
+++ src/views/workstations/edit/index.js
@@ -1,22 +1,22 @@
 import React from 'react'
 import { history } from '../../../history'
-import { AlertError, AlertLoading, AlertSuccess } from '../../../components/custom'
+import { AlertError, AlertLoading, AlertSuccess, Error } from '../../../components/custom'
+import { LoadingSpinner } from '../../../components/@vuexy/Spinner'
+
 import { usePostResources, useFetchResources } from '../../../utility/customHooks/resources'
 import EditUI from './_editUI'
-import { urlApi } from '../../../utility/helpers/consts'
+import { urlApi, baseApiUrl } from '../../../utility/helpers/consts'
 
 const Edit = (props) => {
   const url = `${urlApi}/api/v1/workstations`
   const { data: { loading, error, items }, update, clean } = usePostResources()
-  const { items: areas } = useFetchResources(`${urlApi}/api/v1/areas?all`)
+  const { items: areas } = useFetchResources(`${urlApi}${baseApiUrl}areas?all`)
   const { items: data } = areas
+  if (loading) return <LoadingSpinner />
+  if (error) return <Error message={error} />
   return (
     <>
-      {
-        data && (
-          <EditUI handleSubmit={(values) => update({ name: values.name, description: values.description, ...(values.information && { information: values.information }), area_id: values.area_id }, `${url}/${values.id}`)} {...props} areas={data.data.data} />
-        )
-      }
+      <EditUI handleSubmit={(values) => update({ name: values.name, description: values.description, ...(values.information && { information: values.information }), area_id: values.area_id }, `${url}/${values.id}`)} {...props} areas={data} />
       {loading && <AlertLoading message="Actualizando puesto de trabajo" />}
       {error && <AlertError error={error} callback={() => clean()} />}
       {items && (
