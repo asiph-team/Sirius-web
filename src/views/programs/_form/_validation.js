aa1a73fe4d7b58355218cabc07338d25d7e0bb9a
Diego Arrieta <darrieta.lnx@gmail.com>
2021-01-23 11:59:17 -0300

    validations on edit and add

--- src/views/programs/_form/_validation.js
+++ src/views/programs/_form/_validation.js
@@ -3,7 +3,7 @@ import * as Yup from 'yup'
 const today = new Date()
 today.setHours(0, 0, 0, 0)
 
-export const validationSchema = Yup.object().shape({
+export const validationSchemaAdd = Yup.object().shape({
   name: Yup.string()
     .min(3, 'El nombre de la vigilancia médica debe ser mayor a 3 caractéres')
     .max(100, 'El nombre de la vigilancia médica debe ser menor a 100 caractéres')
@@ -17,6 +17,29 @@ export const validationSchema = Yup.object().shape({
     .required('La frecuencia es requerida'),
   start_date: Yup.date()
     .min(today, 'La fecha de compromiso no debe ser menor a la fecha actual')
+    .required('La fecha de compromiso es requerida'),
+  end_date: Yup.date()
+    // eslint-disable-next-line camelcase
+    .when('start_date', (start_date, yup) => start_date && yup.min(start_date, 'La fecha final no puede ser antes de la inicial'))
+    .required('La fecha final es requerida'),
+  employees_id: Yup.string()
+    .ensure()
+    .required('Empleados invitados es requerido'),
+})
+
+export const validationSchemaEdit = Yup.object().shape({
+  name: Yup.string()
+    .min(3, 'El nombre de la vigilancia médica debe ser mayor a 3 caractéres')
+    .max(100, 'El nombre de la vigilancia médica debe ser menor a 100 caractéres')
+    .required('El nombre de la vigilancia médica es requerido'),
+  description: Yup.string()
+    .min(5, 'La descripción debe ser mayor a 5 caractéres')
+    .max(300, 'La descripción debe ser menor a 80 caractéres')
+    .required('Una descripción es requerida'),
+  frequency: Yup.string()
+    .ensure()
+    .required('La frecuencia es requerida'),
+  start_date: Yup.date()
     .required('La fecha de compromiso es requerida'),
   end_date: Yup.date()
     // eslint-disable-next-line camelcase
