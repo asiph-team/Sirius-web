3ab6c09ccacab01bbfcf419a41f767553cf8ea3e
Diego Arrieta <darrieta.lnx@gmail.com>
2021-02-03 09:30:03 -0300

    host

--- src/views/auth/Login/index.js
+++ src/views/auth/Login/index.js
@@ -8,13 +8,12 @@ import {
   UncontrolledAlert,
 } from 'reactstrap'
 import '../../../assets/scss/pages/authentication.scss'
-import { useLocation } from 'react-router-dom'
 import logoImg from '../../../assets/img/logo/logo-brand.png'
 import { ContextAuth } from '../../../utility/context/Auth'
 import LoginForm from './_LoginForm'
 
 const Login = () => {
-  const url = typeof window !== 'undefined' ? window.location.href : '.'
+  const url = typeof window !== 'undefined' ? window.location.host : '.'
   return (
     <Row className="m-0 justify-content-center">
       <Col lg="4" md="5" sm="7" xs="10" className="d-flex justify-content-center">
