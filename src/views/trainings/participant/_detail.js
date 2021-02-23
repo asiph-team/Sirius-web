8f9fd0e5e96b8f59eca88271f3e3bb5a43b2b3cf
Diego Arrieta <darrieta.lnx@gmail.com>
2021-02-23 18:49:39 -0300

    improve on function

--- src/views/trainings/participant/_detail.js
+++ src/views/trainings/participant/_detail.js
@@ -17,21 +17,16 @@ const DetailUI = (props) => {
     setSelected(item)
     setVisibility({ ...visibility, [type]: visible })
   }
-  const transformData = () => {
-    const newData = data.data.data.map((item) => {
-      return {
-        id: item.id,
-        name: `${item.user.name} ${item.user.lastname}`,
-        rut: item.user.rut,
-        area: item.user.area ? item.user.area.name : null,
-        workstation: item.user.workstation ? item.user.workstation.name : null,
-        signature: item.signature,
-        date_signature: item.date_signature ? moment(item.date_signature, 'DD/MM/YYYY HH:mm:ss').format('DD/MM/YYYY') : '',
-        assistance: item.status === 'invited' ? 'Invitado' : 'Asistió',
-      }
-    })
-    return newData
-  }
+  const transformData = () => data.data.data.map((item) => ({
+    id: item.id,
+    name: `${item.user.name} ${item.user.lastname}`,
+    rut: item.user.rut,
+    area: item.user.area ? item.user.area.name : null,
+    workstation: item.user.workstation ? item.user.workstation.name : null,
+    signature: item.signature,
+    date_signature: item.date_signature ? moment(item.date_signature, 'DD/MM/YYYY HH:mm:ss').format('DD/MM/YYYY') : '',
+    assistance: item.status === 'invited' ? 'Invitado' : 'Asistió',
+  }))
   return (
     <>
       <HeaderSign detail={training} title="cursos" relator show={show} />
