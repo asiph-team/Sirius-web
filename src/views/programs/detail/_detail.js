9599c47a0143cfeda58ee22a83498b453a1a365c
Diego Arrieta <darrieta.lnx@gmail.com>
2021-04-15 17:44:37 -0400

    fix VM

--- src/views/programs/detail/_detail.js
+++ src/views/programs/detail/_detail.js
@@ -24,6 +24,7 @@ const DetailUI = (props) => {
             data={data.data.data}
             show={show}
             url="/dashboard/programs/participants"
+            module="programs"
           />
         )
       }
